ced587fd41cf62f466fb6f30ab57df72b6ed3c0a
alireza <alireza.lm69@gmail.com>
2021-04-11 01:57:48 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+
+from flask import Flask, jsonify, request
+import os, sys
+
+app = Flask(__name__)
+
+PATH = os.path.join(os.path.dirname(__file__))
+sys.path.append(PATH)
+
+
+@app.route('/', methods = ['POST'])
+def main_page():
+    if request.method == 'POST':
+        problem_data = json.loads(request.get_data())
+        with open('config.json', 'w') as jsonfile:
+            json.dump(problem_data, jsonfile)
+
+        mpi_run = os.system(f"mpiexec -n {problem_data['nNodes']} {sys.executable} {PATH}/run.py")
+        print(sys.path)
+        with open('solution.json') as jsonfile:
+            my_data = json.load(jsonfile)
+    return jsonify(my_data)
+
+
+if __name__ == '__main__':
+    app.run(debug = True, host = '127.0.0.1', port = '5000')
---
+++ run.py
@@ -0,0 +1,32 @@
+import json
+import sys
+
+from flask import json
+from mpi4py import MPI
+
+from dccp.problem.problem import Problem
+
+with open('config.json') as jsonfile:
+    my_data = json.load(jsonfile)
+
+print(my_data)
+
+
+def run(data):
+    comm = MPI.COMM_WORLD
+
+    rank = comm.Get_rank()
+
+    print(data, f"from node {rank}")
+
+    problem = Problem(problem_data = data).create_random_problem_instance(0.9)
+    solution_data = problem.solve(comm, MPI)
+
+    if rank == 0:
+        with open('solution.json', 'w') as json_ans:
+            json.dump(solution_data, json_ans)
+        return solution_data
+
+
+if __name__ == '__main__':
+    run(my_data)
