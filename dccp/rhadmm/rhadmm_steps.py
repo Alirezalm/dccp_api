e38c92e1981307f6a52a568be7a678e39419050c
alireza <alireza.lm69@gmail.com>
2021-05-09 23:26:59 -0300

    gurobi for qcp implemented

--- dccp/rhadmm/rhadmm_steps.py
+++ dccp/rhadmm/rhadmm_steps.py
@@ -3,6 +3,8 @@ from numpy.linalg import norm
 from numpy.random import randn
 from scipy.optimize import minimize, NonlinearConstraint, BFGS
 
+from dccp.rhadmm.gurobi_qcp import gurobi_qcp
+
 
 def update_primary_vars(rhadmm_obj, rhadmm_grad, n_vars, constrs = None):
     initial_condition = zeros((n_vars,))
@@ -31,12 +33,12 @@ def update_primary_vars(rhadmm_obj, rhadmm_grad, n_vars, constrs = None):
         else:
             raise ValueError("INNER PROBLEM FAILED: PROBLEM INFEASIBLE")
 
-        # x = interior_point
-    # solver = minimize(rhadmm_obj, jac = rhadmm_grad, x0 = initial_condition, method = method, options = options,
-    #                   constraints = quad_constr)
+        x = gurobi_qcp(rhadmm_obj, quad_constr[0].fun, init = initial_condition)
+    solver = minimize(rhadmm_obj, jac = rhadmm_grad, x0 = initial_condition, method = method, options = options,
+                      constraints = quad_constr)
     # print(solver)
-    # if solver.success:
-    #
-    #     return solver.x.reshape(n_vars, 1)
-    # else:
-    #     raise ValueError(f"Inner Solver inside RHADMM failed. {method}")
+    if solver.success:
+
+        return solver.x.reshape(n_vars, 1)
+    else:
+        raise ValueError(f"Inner Solver inside RHADMM failed. {method}")
