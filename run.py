505d5fad0ec36b252f66751a5b959d8ff011f70a
alireza <alireza.lm69@gmail.com>
2021-05-27 22:51:18 -0300

    initial commit

--- run.py
+++ run.py
@@ -1,11 +1,6 @@
-import json
-from flask import json
 from mpi4py import MPI
+
 from dccp.problem.problem import Problem
-
-
-with open('config.json') as jsonfile:
-    my_data = json.load(jsonfile)
 
 
 def run(data):
@@ -16,11 +11,6 @@ def run(data):
     problem = Problem(problem_data = data).create_random_problem_instance(0.16)
     solution_data = problem.solve(comm, MPI)
 
-    if rank == 0:
-        with open('solution.json', 'w') as json_ans:
-            json.dump(solution_data, json_ans)
-        return solution_data
 
 
-if __name__ == '__main__':
-    run(my_data)
+
